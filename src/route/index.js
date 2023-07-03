85079724acdc46ecf1153f336448141118304a7a
bakulina <bakulinaelena800@gmail.com>
2023-07-03 16:49:12 +0200

    add

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,31 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Olena',
+    lastname: 'Bakulina',
+  },
+  position: 'developer',
+  salary: ' 200000',
+  address: 'Ukrain, Odessa',
+};
+var footer = {
+  social: {
+    email: {
+      text: 'bakulinaelena800@gmail.com',
+      href: 'bakulinaelena800@gmail.com',
+    },
+    phone: {
+      text: '+420773164125',
+      href: 'tel:+420773164125',
+
+    },
+  },
+}
+
+//==================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -12,7 +37,12 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('', {
+
+
+
+
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
@@ -23,10 +53,136 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'resume',
+    },
+    header,
+
+    main: {
+      summary: {
+        title: 'summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+        work on a new project I learn the domain and try to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`
+      },
+      experience: {
+        title: 'Other experience',
+        text: ` Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc.`
+      },
+    },
+    footer,
+
+
+
   })
 })
 
 // ================================================================
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'resume',
+    },
+    header,
+
+    main: {
+      certificates: [
+        {
+          name: 'test',
+          id: '345567',
+
+        }
+      ],
+      educations: [
+        {
+          name: 'дет сад',
+          point: 10,
+          isEnd: true,
+        },
+        {
+          name: 'школа',
+          point: 9,
+          isEnd: false,
+        },
+        {
+          name: 'университет',
+          point: 10,
+          isEnd: true,
+        },
+        {
+          name: 'дикрет',
+          point: 7,
+          isEnd: false,
+        },
+        {
+          name: 'работа',
+          point: 6,
+          isEnd: true,
+        },
+
+
+      ],
+    },
+    footer,
+
+
+  })
+})
+
+// ================================================================
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'resume',
+    },
+    header,
+    main: {
+      skills: [
+        {
+          name: 'html',
+          point: 10,
+          istop: true,
+        },
+        {
+          name: 'handlebars',
+          point: 10,
+        },
+        {
+          name: 'vs code',
+          point: 9,
+        },
+        {
+          name: 'git',
+          point: 6,
+        },
+        {
+          name: 'terminal',
+          point: 10,
+        },
+        {
+          name: 'php',
+          point: null,
+        },
+
+        {
+          name: 'react.js',
+          point: 0,
+        },
+      ],
+    },
+    footer,
+
+
+  })
+})
+
 
 // Підключаємо роутер до бек-енду
 module.exports = router
+
