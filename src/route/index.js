a6af125d58f0759206ae2ba4beb72763b732f37d
bakulina <bakulinaelena800@gmail.com>
2023-07-15 11:55:23 +0200

    hj

--- src/route/index.js
+++ src/route/index.js
@@ -315,7 +315,7 @@ router.get('/shoporder', function (req, res) {
     },
     action_block: [
       { text: 'Швидке замовлення', isSuccess: true },
-      { text: 'Видалити', isDanger: true },
+      { text: 'Видалити', isSuccess: true },
       { text: 'Детальніше', isInfo: true },
     ],
     total: {
